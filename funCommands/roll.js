688c994d4b132dfc64c1a76c19603c5db68d0af4
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:35:05 +0700

    roll.js

--- funCommands/roll.js
+++ funCommands/roll.js
@@ -2,12 +2,12 @@ const db = require("../mongodb");
 module.exports = {
   name: 'roll',
   aliases: ['dice', 'rolldice'],
-  description: 'Rolls a dice',
+  description: 'Đổ xúc xắc',
   execute(message, args) {
-    const sides = parseInt(args[0]) || 6; // Default to 6 sides if no argument provided
+    const sides = parseInt(args[0]) || 6; // Mặc định là 6 mặt nếu không cung cấp đối số
     const result = Math.floor(Math.random() * sides) + 1;
 
     // Reply to the user with the dice roll result
-    message.reply(`You rolled a ${result} on a ${sides}-sided dice!`);
+    message.reply(`Bạn đã lăn một ${result} trên một ${sides}-xúc xắc hai mặt!`);
   },
 };
