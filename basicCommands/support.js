c1501daac13be59b16380d3d98a079948f4f3343
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:50:13 +0700

    support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,19 +4,19 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.com/invite/mVUMG7tdRa";
+      const githubLink = "https://github.com/MinDz204";
+      const replitlink = "https://replit.com/@khanhtq2205";
+      const youtubeLink = "https://www.youtube.com/@YuongzMin";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              iconURL: 'https://cdn.discordapp.com/attachments/1242855743955275919/1247580140699914270/standard.gif?ex=66608b0c&is=665f398c&hm=5fd8d349cd2eef361ad95e1ffff38c3df85ac1201fa233a4d9f2d707601ebed8&', 
+              url: 'https://discord.com/invite/mVUMG7tdRa'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Tham gia máy chủ Discord của chúng tôi để được hỗ trợ và cập nhật:**\n- Discord - ${supportServerLink}\n\n➡️ **Theo dõi chúng tôi tại:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1242855743955275919/1247580140699914270/standard.gif?ex=66608b0c&is=665f398c&hm=5fd8d349cd2eef361ad95e1ffff38c3df85ac1201fa233a4d9f2d707601ebed8&')
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
