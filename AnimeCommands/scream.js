9d24bc7acb5a861df6e180f92fbabb619b3f9256
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-05 00:21:08 +0700

    scream.js

--- AnimeCommands/scream.js
+++ AnimeCommands/scream.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'scream',
-  description: 'Scream in fear or excitement!',
+  description: 'Hét lên vì sợ hãi hoặc phấn khích!',
   async execute(message, args) {
     const sender = message.author;
     const screamGif = await anime.scream();
 
     const embed = new EmbedBuilder()
       .setColor('#ff3300')
-      .setDescription(`${sender} is screaming... 😱`)
+      .setDescription(`${sender} đang la hét... 😱`)
       .setImage(screamGif);
 
     message.reply({ embeds: [embed] });
