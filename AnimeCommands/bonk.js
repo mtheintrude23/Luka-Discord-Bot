9ce749b05cac607de85f1b13ac42fa1b3d787147
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:55:25 +0700

    bonk.js

--- AnimeCommands/bonk.js
+++ AnimeCommands/bonk.js
@@ -3,7 +3,7 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'bonk',
-  description: 'Bonk someone on the head!',
+  description: 'Đập đầu ai đó!',
   async execute(message, args) {
     const sender = message.author;
     const targetUser = message.mentions.users.first();
@@ -11,7 +11,7 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setColor('#ff3366')
-      .setDescription(`${sender} bonks ${targetUser || 'the air'} on the head! 🤦‍♂️`)
+      .setDescription(`${sender} bonks ${targetUser || 'không khí'} trên đầu! 🤦‍♂️`)
       .setImage(bonkGif);
 
     message.reply({ embeds: [embed] });
