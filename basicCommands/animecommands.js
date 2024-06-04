1ac41ea4784c0ae227a35af8049f7f541b32f9cb
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:39:02 +0700

    animecommands.js

--- basicCommands/animecommands.js
+++ basicCommands/animecommands.js
@@ -2,12 +2,12 @@ const { EmbedBuilder } = require('discord.js');
 const db = require("../mongodb");
 module.exports = {
   name: 'animecommands',
-  description: 'Display a list of available anime-related commands!',
+  description: 'Hiển thị danh sách các lệnh có sẵn liên quan đến anime!',
   execute(message, args) {
     const embed = new EmbedBuilder()
       .setColor('#FFFFFF')
-      .setTitle('Anime Commands')
-      .setDescription(`__**✅ List Of Availabe Interations**__\n\n▶️ __**Section 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Section 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
+      .setTitle('Lệnh Anime')
+      .setDescription(`__**✅ Danh sách tương tác có sẵn**__\n\n▶️ __**Phần 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Phần 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
       
       .setImage(`https://cdn.discordapp.com/attachments/1140841446228897932/1142126954775068762/pxfuel.jpg`);
     message.reply({ embeds: [embed] });
