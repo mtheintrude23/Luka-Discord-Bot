f1dcc8a777f1e63259add4b62f471760cf937aa4
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-05 00:19:57 +0700

    sad.js

--- AnimeCommands/sad.js
+++ AnimeCommands/sad.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 
 module.exports = {
   name: 'sad',
-  description: 'Express sadness with a GIF!',
+  description: 'Thể hiện nỗi buồn bằng ảnh GIF!',
   async execute(message, args) {
     const sender = message.author;
     const sadGif = await anime.sad();
 
     const embed = new EmbedBuilder()
       .setColor('#3366ff')
-      .setDescription(`${sender} is feeling sad... 😢`)
+      .setDescription(`${sender} cảm thấy buồn... 😢`)
       .setImage(sadGif);
 
     message.reply({ embeds: [embed] });
