e23539c92e8e8fd1aa491d2b297f86ce9ab2165c
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-05 00:12:13 +0700

    thinking.js

--- AnimeCommands/thinking.js
+++ AnimeCommands/thinking.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'thinking',
-  description: 'Show that you\'re thinking!',
+  description: 'Cho thấy rằng bạn\'đang suy nghĩ lại!',
   async execute(message, args) {
     const sender = message.author;
     const thinkingGif = await anime.thinking();
 
     const embed = new EmbedBuilder()
       .setColor('#99ccff')
-      .setDescription(`${sender} is thinking... 🤔`)
+      .setDescription(`${sender} đang nghĩ... 🤔`)
       .setImage(thinkingGif);
 
     message.reply({ embeds: [embed] });
