f66f2e60e3ffa3b472ca24c975698fe061d37e13
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:16:52 +0700

    userinfo.js

--- utilityCommands/userinfo.js
+++ utilityCommands/userinfo.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     name: 'userinfo',
     aliases: ['user', 'uinfo'],
-    description: 'Get information about a user',
+    description: 'Nhận thông tin của bản thân',
     execute(message, args) {
         // Check if a user was mentioned in the command, or use the message author as the default user
         const user = message.mentions.users.first() || message.author;
@@ -12,12 +12,12 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setColor('#FFFFFF')
         .setAuthor({
-        name: 'User Info!',
+        name: 'Thông Tin Bản Thân!',
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144684108174348318/giphy_1.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://discord.com/invite/mVUMG7tdRa'
     })
         .setThumbnail(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
-        .setDescription(`**Username**:  ${user.username}\n**Discriminator**:  ${user.discriminator}\n**ID**:  ${user.id}\n**Joined Discord**:  ${user.createdAt.toUTCString()}`)
+        .setDescription(`**Tên**:  ${user.username}\n**Discriminator**:  ${user.discriminator}\n**ID**:  ${user.id}\n**Đã Tham Gia Vào Discord**:  ${user.createdAt.toUTCString()}`)
         .setTimestamp();
 
         message.reply({ embeds: [embed] });
