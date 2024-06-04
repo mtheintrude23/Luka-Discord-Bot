2ce7ded1b3c50f98502ccbe8378d3d813a058b18
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-05 00:18:04 +0700

    nervous.js

--- AnimeCommands/nervous.js
+++ AnimeCommands/nervous.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'nervous',
-  description: 'Show that you\'re nervous!',
+  description: 'Cho thấy bạn đang lo lắng!',
   async execute(message, args) {
     const sender = message.author;
     const nervousGif = await anime.nervous();
 
     const embed = new EmbedBuilder()
       .setColor('#cc9900')
-      .setDescription(`${sender} is feeling nervous... 😰`)
+      .setDescription(`${sender} đang cảm thấy lo lắng... 😰`)
       .setImage(nervousGif);
 
     message.reply({ embeds: [embed] });
