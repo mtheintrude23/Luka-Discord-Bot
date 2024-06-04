70e1de4f2098de4199dff08dbee6209368c726c2
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-05 00:05:39 +0700

    yes.js

--- AnimeCommands/yes.js
+++ AnimeCommands/yes.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'yes',
-  description: 'Express agreement or approval!',
+  description: 'Thể hiện sự đồng ý hoặc phê duyệt!',
   async execute(message, args) {
     const sender = message.author;
     const yesGif = await anime.yes();
 
     const embed = new EmbedBuilder()
       .setColor('#00cc66')
-      .setDescription(`${sender} says yes! 👍`)
+      .setDescription(`${sender} nói có! 👍`)
       .setImage(yesGif);
 
     message.reply({ embeds: [embed] });
