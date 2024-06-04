e7c692ae2350098c9258359f65bbef7b30a8bab1
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:57:02 +0700

    bored.js

--- AnimeCommands/bored.js
+++ AnimeCommands/bored.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'bored',
-  description: 'Show that you\'re bored!',
+  description: 'Cho thấy rằng bạn\'chán quá!',
   async execute(message, args) {
     const sender = message.author;
     const boredGif = await anime.bored();
 
     const embed = new EmbedBuilder()
       .setColor('#996633')
-      .setDescription(`${sender} is feeling bored... 😴`)
+      .setDescription(`${sender} đang cảm thấy chán... 😴`)
       .setImage(boredGif);
 
     message.reply({ embeds: [embed] });
