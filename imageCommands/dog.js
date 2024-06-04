9ebb727727f4291109ce2bccc89e583e4a538222
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 23:29:38 +0700

    dog.js

--- imageCommands/dog.js
+++ imageCommands/dog.js
@@ -4,27 +4,27 @@ const db = require("../mongodb");
 module.exports = {
   name: 'dog',
   aliases: ['randomdog', 'woof'],
-  description: 'Displays a random dog image',
+  description: 'Hiển thị hình ảnh con chó ngẫu nhiên',
   async execute(message, args) {
     try {
       const response = await axios.get('https://dog.ceo/api/breeds/image/random');
       const imageUrl = response.data.message;
 
       if (!imageUrl) {
-        throw new Error('Failed to fetch dog image.');
+        throw new Error('Không tìm nạp được hình ảnh con chó.');
       }
 
       const embed = new EmbedBuilder()
         .setColor('#FFFFFF')
-        .setTitle('Random Dog Image 🐶')
+        .setTitle('Hình ảnh con chó ngẫu nhiên 🐶')
         .setImage(imageUrl);
 
       // Reply to the user who executed the command
       message.reply({ embeds: [embed] });
     } catch (error) {
-      console.error('Error fetching dog image:', error);
+      console.error('Lỗi tìm nạp hình ảnh con chó:', error);
       // Reply to the user who executed the command
-      message.reply('Sorry, I couldn\'t fetch a dog image at the moment.');
+      message.reply('Rất tiếc, hiện tại tôi không thể tìm hình ảnh con chó.');
     }
   },
 };
